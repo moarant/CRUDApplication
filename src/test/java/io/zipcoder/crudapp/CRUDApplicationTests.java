e680ce6ef87432e5cbaa99a0de5c977fa6a05816
Molly Arant <mccormick.mollyt@gmail.com>
2017-06-16 12:04:05 -0400

    completed core logic of lab

--- src/test/java/io/zipcoder/crudapp/CRUDApplicationTests.java
+++ src/test/java/io/zipcoder/crudapp/CRUDApplicationTests.java
@@ -2,15 +2,29 @@ package io.zipcoder.crudapp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=CRUDApplication.class)
 @SpringBootTest
 public class CRUDApplicationTests {
 
+	@Autowired
+	PersonController personController;
+
 	@Test
 	public void contextLoads() {
+		assertThat(personController).isNotNull();
 	}
+
+//	@Test
+//	public void
 
 }
